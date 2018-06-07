d7919970ee6d5536b26c21c49639fb236a996e2f
Ichsan <ahmdichsanb@gmail.com>
2018-06-07 23:19:46 +0700

    First uploaded soal2
    soal 2 first upload

---
+++ Soal_2/soal2/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// import Satu from './component/Satu';
+
+class App extends Component 
+{
+  render() 
+  {
+    return (
+      // <div>
+      //   <Satu/>
+      // </div>
+
+      <div>
+        
+      </div>
+
+      
+    );
+  }
+}
+export default App;
---
+++ Soal_2/soal2/src/component/Satu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Satu extends Component {
+  render() {
+    return (
+      <div>
+        Ini halaman 1
+      </div>
+    );
+  }
+}
+export default Satu;
